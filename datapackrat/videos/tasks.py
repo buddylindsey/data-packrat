fa79b3b54d705c8a89a28341c3f6646d5a051718
Buddy Lindsey, Jr <buddy@picks.co>
2018-05-08 20:54:41 -0500

    Move managment command to be a celery task

--- datapackrat/videos/tasks.py
+++ datapackrat/videos/tasks.py
@@ -1,8 +1,10 @@
 import requests
+import youtube_dl
 
 from django.conf import settings
 from celery import shared_task
 
+from videos.models import Video, VideoCategory
 from .models import  Channel, Video
 
 
@@ -36,3 +38,46 @@ class LatestChannelVideo:
 def find_latest_channel_videos():
     lcv = LatestChannelVideo()
     lcv.run()
+
+
+class PullVideoData:
+    def get_location(self, video):
+        location = settings.DOWNLOAD_FOLDER.format(category=video.category.slug)
+
+        if video.playlist:
+            location += '{}/'.format(video.playlist.slug)
+
+        location += settings.DOWNLOAD_FILE_FORMAT
+
+        return location
+
+    def run(self):
+
+        ydl_opts = {
+            'format': 'bestvideo+bestaudio',
+            'writesubtitles': True,
+            'subtitlesformat': 'srt',
+            'ffmpeg_location': settings.FFMPEG_LOCATION,
+            'prefer_ffmpeg': True,
+            'postprocessors': [
+                { 'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4' },
+                { 'key': 'FFmpegMetadata' }
+            ],
+        }
+
+        for video in Video.objects.filter(status=Video.IN_QUEUE):
+
+            ydl_opts['outtmpl'] = self.get_location(video)
+
+            try:
+                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([video.target])
+                video.status = Video.COMPLETED
+            except:
+                video.status = Video.ERROR
+            video.save()
+
+@shared_task
+def pull_video_data():
+    pvd = PullVideoData()
+    pvd.run()
