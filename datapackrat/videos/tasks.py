c19091f435834b9dcefd887843d09970c3e2c589
Buddy Lindsey, Jr <buddy@picks.co>
2018-05-13 14:21:48 -0500

    Convert pulling a video over to a single method, and send all downloads to an actual queue instead of one really long process

--- datapackrat/videos/tasks.py
+++ datapackrat/videos/tasks.py
@@ -8,6 +8,51 @@ from videos.models import Video, VideoCategory
 from .models import  Channel, Video
 
 
+class GetSingleVideo:
+    def __init__(self, video_id, force=False):
+        self.video = Video.objects.get(id=video_id)
+        self.force = force
+
+    def get_location(self):
+        location = settings.DOWNLOAD_FOLDER.format(
+            category=self.video.category.slug)
+
+        if self.video.playlist:
+            location += '{}/'.format(self.video.playlist.slug)
+
+        location += settings.DOWNLOAD_FILE_FORMAT
+
+        return location
+
+    def run(self):
+        ydl_opts = {
+            'format': 'bestvideo+bestaudio',
+            'writesubtitles': True,
+            'subtitlesformat': 'srt',
+            'ffmpeg_location': settings.FFMPEG_LOCATION,
+            'prefer_ffmpeg': True,
+            'postprocessors': [
+                { 'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4' },
+                { 'key': 'FFmpegMetadata' }
+            ],
+        }
+
+        ydl_opts['outtmpl'] = self.get_location()
+
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([self.video.target])
+            self.video.status = Video.COMPLETED
+        except:
+            self.video.status = Video.ERROR
+        self.video.save()
+
+@shared_task
+def get_single_video(video_id, force=False):
+    gsv = GetSingleVideo(video_id, force)
+    gsv.run()
+
+
 class LatestChannelVideo:
     def run(self):
         for channel in Channel.objects.filter(paused=False).iterator():
@@ -41,41 +86,10 @@ def find_latest_channel_videos():
 
 
 class PullVideoData:
-    def get_location(self, video):
-        location = settings.DOWNLOAD_FOLDER.format(category=video.category.slug)
-
-        if video.playlist:
-            location += '{}/'.format(video.playlist.slug)
-
-        location += settings.DOWNLOAD_FILE_FORMAT
-
-        return location
-
     def run(self):
-
-        ydl_opts = {
-            'format': 'bestvideo+bestaudio',
-            'writesubtitles': True,
-            'subtitlesformat': 'srt',
-            'ffmpeg_location': settings.FFMPEG_LOCATION,
-            'prefer_ffmpeg': True,
-            'postprocessors': [
-                { 'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4' },
-                { 'key': 'FFmpegMetadata' }
-            ],
-        }
-
         for video in Video.objects.filter(status=Video.IN_QUEUE):
+            get_single_video.delay(video.id)
 
-            ydl_opts['outtmpl'] = self.get_location(video)
-
-            try:
-                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                    ydl.download([video.target])
-                video.status = Video.COMPLETED
-            except:
-                video.status = Video.ERROR
-            video.save()
 
 @shared_task
 def pull_video_data():
